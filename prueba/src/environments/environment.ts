80fd5f8c518cca9dd91b54a48ebc8ee8b6817551
Ivan <ivan.palafox@softtek.com>
2024-12-11 15:25:59 -0600

    Arreglo de rama principal

---
+++ prueba/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://api.github.com',
+    apiKey: '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',
+
+    //Se agrega la clave de acceso
+  };  
